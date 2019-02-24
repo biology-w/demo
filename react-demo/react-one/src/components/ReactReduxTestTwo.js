e0522f3ea90e5d6fbe0c6232937ccddc28cf0659
1185279859@qq.com <1185279859@qq.com>
2019-02-24 11:19:45 +0800

    modified react-redux

--- react-demo/react-one/src/components/ReactReduxTestTwo.js
+++ react-demo/react-one/src/components/ReactReduxTestTwo.js
@@ -7,17 +7,26 @@ import store from '../store/store'
 // 装饰器
 @connect(
   state => ({state}),
-  dispatch => ({
-    add: () => dispatch({ type: 'ADD' }),
-    minus: () => dispatch({ type: 'MINUS' })
-  })
+  {
+    add: () => ({type: 'ADD'}),
+    minus: () => ({type: 'MINUS'})
+  }
 )
+
+// or
+// @connect(
+//   state => ({state}),
+//   dispatch => ({
+//     add: () => dispatch({ type: 'ADD' }),
+//     minus: () => dispatch({ type: 'MINUS' })
+//   })
+// )
 class ReactReduxTest extends Component {
   render() {
     return (
       <div>
           <h1>React-Redux test-Two</h1>
-        <p>{this.props.state}</p>
+        <p>{this.props.state}</p> 
         <Button onClick={() => this.props.minus()}>-</Button>
         <Button onClick={() => this.props.add()}>+</Button>
       </div>
